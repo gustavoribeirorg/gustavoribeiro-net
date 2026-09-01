ef7ad8525688f4d7dd8754f6948225d454e95b5e
Gustavo Ribeiro <jornalistagustavoribeiro@gmail.com>
2026-09-01 15:28:58 -0300

    remove: deletando posts

--- scripts/fix_ruby_sdk.rb
+++
@@ -1,9 +0,0 @@
-# Script auxiliar para compatibilizar o Ruby 2.6 com o SDK do macOS moderno (darwin24 -> darwin25)
-require 'rbconfig'
-if RbConfig::CONFIG['rubyarchhdrdir'] && !File.exist?(RbConfig::CONFIG['rubyarchhdrdir'])
-  fixed = RbConfig::CONFIG['rubyarchhdrdir'].sub('darwin24', 'darwin25')
-  if File.exist?(fixed)
-    RbConfig::CONFIG['rubyarchhdrdir'] = fixed
-    RbConfig::MAKEFILE_CONFIG['rubyarchhdrdir'] = fixed
-  end
-end
